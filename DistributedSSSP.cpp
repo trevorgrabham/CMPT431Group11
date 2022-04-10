65c799d4a06001fd25b072d2d7bae01d5e72c486
asorour <asorour@sfu.ca>
2022-04-10 13:04:57 -0700

    working serial draft (STL)

--- DistributedSSSP.cpp
+++ DistributedSSSP.cpp
@@ -1,113 +1,6 @@
 #include "core/graph.h"
 #include <queue>
 
-/* Indexed Minimum Prority Queue Class
-// Adapted from: https://www.geeksforgeeks.org/indexed-priority-queue-with-implementation/
-class indexed_min_priority_queue {
-  // Storing indices of values using key
-  std::unordered_map<uintV, uintV, std::hash<uintV>> m;
-  std::vector<std::pair<uintV, uintV> > v; // underlying container
-  uintV numElements;
-  uintV infinity; // defintion for infinity
-
-  // heapify the container
-  void heapify(std::vector<std::pair<uintV, uintV> >& v, uintV heap_size, uintV index) {
-    uintV leftChild = 2 * index + 1;
-    uintV rightChild = 2 * index + 2;
-    uintV suitableNode = index;
-
-    if (leftChild < heap_size && v[suitableNode].second > v[leftChild].second) {
-        suitableNode = leftChild;
-    }
-
-    if (rightChild < heap_size && v[suitableNode].second > v[rightChild].second) {
-        suitableNode = rightChild;
-    }
-
-    if (suitableNode != index) {
-      // swap the value
-      std::pair<uintV, uintV> temp = v[index];
-      v[index] = v[suitableNode];
-      v[suitableNode] = temp;
-
-      // updating the map
-      m[v[index].first] = index + 1;
-      m[v[suitableNode].first] = suitableNode + 1;
-
-      // heapify other affected nodes
-      heapify(v, numElements, suitableNode);
-    }
-  }
-  
-public:
-  indexed_min_priority_queue(uintV _infinity) : infinity(_infinity), numElements(0) {
-    m.clear();
-    v.clear();
-  }
-
-  void push(uintV key, uintV value) {
-    // Adding element
-    v.push_back(std::make_pair(key, value));
-    numElements++;
-    m[key] = numElements;
-
-    uintV index = numElements - 1;
-
-    // Comparing to parent node
-    while (index != 0 && v[(index - 1) / 2].second > v[index].second) {
-      // swap the value
-      std::pair<uintV, uintV> temp = v[index];
-      v[index] = v[(index - 1) / 2];
-      v[(index - 1) / 2] = temp;
-
-      // updating the map
-      m[v[index].first] = index + 1;
-      m[v[(index - 1) / 2].first] = (index - 1) / 2 + 1;
-
-      // updating index in map
-      index = (index - 1) / 2;
-    }
-  }
-
-  uintV pop() {
-    uintV top = v[0].first;
-    // Removing element
-    v.erase(v.begin());
-    numElements--;
-    heapify(v, numElements, 0);
-    return top;
-  }
-
-  bool empty() { return numElements == 0 || v[0].second == infinity; }
-
-  void changeAtKey(uintV key, uintV value) {
-    std::cout<<"changeAtKey() BEFORE: v0=<"<<v[0].first<<","<<v[0].second<<">, v1=<"<<v[1].first<<","<<v[1].second<<">, v2=<"<<v[2].first<<","<<v[2].second<<">, v3=<"<<v[3].first<<","<<v[3].second<<">\n";
-
-    uintV index = m[key] - 1;
-    v[index].second = value;
-
-    // Comparing to child nodes
-    heapify(v, numElements, index);
-
-    // Comparing to Parent Node
-    while (index != 0 && v[(index - 1) / 2].second > v[index].second) {
-      // swap the value
-      std::pair<uintV, uintV> temp = v[index];
-      v[index] = v[(index - 1) / 2];
-      v[(index - 1) / 2] = temp;
-
-      // updating the map
-      m[v[index].first] = index + 1;
-      m[v[(index - 1) / 2].first] = (index - 1) / 2 + 1;
-
-      // updating index in map
-      index = (index - 1) / 2;
-    }
-        std::cout<<"changeAtKey() AFTER: v0=<"<<v[0].first<<","<<v[0].second<<">, v1=<"<<v[1].first<<","<<v[1].second<<">, v2=<"<<v[2].first<<","<<v[2].second<<">, v3=<"<<v[3].first<<","<<v[3].second<<">\n";
-  }
-};
-*/
-
 void writeResults(Graph &g, uintV source, std::vector<uintV> const &distance, std::vector<uintV> const &path, std::string input_file_path) {
   std::string output_file_path = "./output" + input_file_path.substr(input_file_path.find_last_of("/"), input_file_path.length()) + "_s" + std::to_string(source) + ".out";
   std::cout << "Writing results to " << output_file_path << "\n";
@@ -120,7 +13,7 @@ void writeResults(Graph &g, uintV source, std::vector<uintV> const &distance, st
 
   for (uintV v = 0; v < g.n_; v++) {
     output_file << "v" << v << ": ";
-    if (path[g.n_] == g.n_) {
+    if (path[v] == g.n_) {
       output_file << "no path found\n";
       continue;
     } else {
